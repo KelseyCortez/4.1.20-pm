cc158fb4f5b1341375a0d542578185147ce0b4ca
Kelsey Cortez <kelscortez@gmail.com>
2020-04-01 19:23:51 -0400

    finished

--- degree_conversion.py
+++ degree_conversion.py
@@ -1,6 +1,11 @@
-Temp = input('Temperature in C? ')
+Temp = float(input('Temperature in C? '))
 
-C = (F - 32)
-F = (C + 32)
 
-if Temp 
+
+Cels_to_Fare = (Temp * (9/5)) + 32
+result = f'{Cels_to_Fare} F'
+
+print(result)
+
+
+
